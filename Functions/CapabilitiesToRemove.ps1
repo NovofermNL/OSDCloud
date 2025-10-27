bf4cd2362eb02739f8e16d6065ffe2eb67273193
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-10-27 22:38:11 +0100

    Delete Functions/CapabilitiesToRemove.ps1

--- Functions/CapabilitiesToRemove.ps1
+++
@@ -1,9 +0,0 @@
-$CapabilitiesToRemove = @(
-    "OpenSSH.Client~~~~0.0.1.0",
-    "XPS.Viewer~~~~0.0.1.0",
-    "Microsoft.Windows.WordPad~~~~0.0.1.0"
-)
-
-foreach ($cap in $CapabilitiesToRemove) {
-    Write-Host "Removing capability: $cap"
-    Remove-WindowsCapability -Online -Name $cap
