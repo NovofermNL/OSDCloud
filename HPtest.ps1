3b5d9ba0682a15201b1e2530ddcaaf9abf3ac191
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-11-02 01:52:20 +0100

    Update HPtest.ps1

--- HPtest.ps1
+++ HPtest.ps1
@@ -175,6 +175,16 @@ Write-Host "Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath"
 Use-WindowsUnattend -Path 'C:\' -UnattendPath $UnattendPath | Out-Null
 
 #================================================
+#    [PostOS] OOBE CMD Command Line
+#================================================
+$OOBECMD = @'
+@echo off
+:: OOBE fase: verwijder standaard apps
+start /wait powershell.exe -NoLogo -ExecutionPolicy Bypass -File C:\Windows\Setup\Scripts\Remove-AppX.ps1
+'@
+$OOBECMD | Out-File -FilePath "$ScriptDir\oobe.cmd" -Encoding ascii -Force
+
+#================================================
 #    [PostOS] SetupComplete
 #================================================
 
