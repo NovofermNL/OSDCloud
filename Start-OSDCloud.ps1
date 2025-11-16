213594715585bcd1e9c0696446a8e8f7bcfec9f3
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-11-16 02:42:13 +0100

    Update Start-OSDCloud.ps1

--- Start-OSDCloud.ps1
+++ Start-OSDCloud.ps1
@@ -113,7 +113,7 @@ $UnattendXml = @"
       language="neutral"
       versionScope="nonSxS"
       xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
-      <InputLocale>0413:00000413</InputLocale>
+      <InputLocale>00413:00020409</InputLocale>
       <SystemLocale>nl-NL</SystemLocale>
       <UserLocale>nl-NL</UserLocale>
       <UILanguage>nl-NL</UILanguage>
