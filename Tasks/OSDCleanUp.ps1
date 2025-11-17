b251e859e72c5a760dd884c41acbc427216c3978
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-11-17 23:46:46 +0100

    Update OSDCleanUp.ps1

--- Tasks/OSDCleanUp.ps1
+++ Tasks/OSDCleanUp.ps1
@@ -13,6 +13,14 @@ If (Test-Path -Path 'C:\OSDCloud\Logs') {
 }
 Move-Item 'C:\ProgramData\OSDeploy\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
 
+# Kopieert de HP-logbestanden
+If (Test-Path -Path 'C:\HP') {
+    Move-Item 'C:\HP\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
+}
+If (Test-Path -Path 'C:\system.sav') {
+    Move-Item 'C:\system.sav\*.*' -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
+}
+
 If (Test-Path -Path 'C:\Temp') {
     Get-ChildItem 'C:\Temp' -Filter *OOBE* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
     Get-ChildItem 'C:\Windows\Temp' -Filter *Events* | Copy-Item -Destination 'C:\ProgramData\Microsoft\IntuneManagementExtension\Logs\OSD' -Force
@@ -22,6 +30,8 @@ If (Test-Path -Path 'C:\Temp') {
 If (Test-Path -Path 'C:\OSDCloud') { Remove-Item -Path 'C:\OSDCloud' -Recurse -Force }
 If (Test-Path -Path 'C:\Drivers') { Remove-Item 'C:\Drivers' -Recurse -Force }
 If (Test-Path -Path 'C:\Intel') { Remove-Item 'C:\Intel' -Recurse -Force }
+If (Test-Path -Path 'C:\HP') { Remove-Item 'C:\HP' -Recurse -Force }
+If (Test-Path -Path 'C:\system.sav') { Remove-Item 'C:\system.sav' -Recurse -Force }
 If (Test-Path -Path 'C:\ProgramData\OSDeploy') { Remove-Item 'C:\ProgramData\OSDeploy' -Recurse -Force }
 
 # Opschonen van scripts
