ca5e1bd32ecac4d929fae30c18cd8153d54e49d3
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2025-11-17 23:37:22 +0100

    Update Add-SetupCompleteScripts.psm1

--- Add-SetupCompleteScripts.psm1
+++ Add-SetupCompleteScripts.psm1
@@ -1,3 +1,8 @@
+# Script nog in test!  
+# Script voegt taken/scripts toe aan bestaande SetupComplete welke door OSDCloud wordt gegenereerd
+
+##########################################################################################################################
+
 Function Set-SetupCompleteCleanUp {
     $ScriptsPath = "$env:SystemRoot\Setup\Scripts"
     $PSFilePath  = Join-Path $ScriptsPath 'SetupComplete.ps1'
@@ -15,3 +20,25 @@ Function Set-SetupCompleteCleanUp {
     Add-Content -Path $PSFilePath -Value 'Write-Output "Completed Create-RunOnce-CleanUp.ps1"'
     Add-Content -Path $PSFilePath -Value 'Write-Output "-------------------------------------------------------------"'
 }
+
+##########################################################################################################################
+
+Function Set-SetupCompleteCMTrace {
+    $ScriptsPath = "$env:SystemRoot\Setup\Scripts"
+    $PSFilePath  = Join-Path $ScriptsPath 'SetupComplete.ps1'
+
+    if (!(Test-Path $ScriptsPath)) {
+        New-Item -Path $ScriptsPath -ItemType Directory -Force | Out-Null
+    }
+
+    if (!(Test-Path $PSFilePath)) {
+        'Write-Output "Start SetupComplete.ps1"' | Set-Content -Path $PSFilePath
+    }
+
+    Add-Content -Path $PSFilePath -Value 'Write-Output "Running CMTrace-Setup.ps1"'
+    Add-Content -Path $PSFilePath -Value '& "$env:SystemRoot\Setup\Scripts\CMTrace-Setup.ps1"'
+    Add-Content -Path $PSFilePath -Value 'Write-Output "Completed CMTrace-Setup.ps1"'
+    Add-Content -Path $PSFilePath -Value 'Write-Output "-------------------------------------------------------------"'
+}
+
+##########################################################################################################################
