b4db6d6bd9837d8981e7edb4725d2b7be0b63e75
NovofermNL <145004153+NovofermNL@users.noreply.github.com>
2026-04-30 22:44:11 +0200

    Update Install-Office365.ps1

--- Software/Office/Install-Office365.ps1
+++ Software/Office/Install-Office365.ps1
@@ -3,7 +3,7 @@ param()
 
 # --- AUTOMATISERING CONFIGURATIE ---
 $ConfigXmlUrl = "https://raw.githubusercontent.com/NovofermNL/OSDCloud/main/Software/Office/Config.xml"
-$OdtExeUrl    = "https://raw.githubusercontent.com/GEBRUIKER/REPO/main/setup.exe"
+$OdtExeUrl    = "https://github.com/NovofermNL/OSDCloud/main/Software/Office/setup.exe"
 $WorkFolder   = "C:\Windows\Temp\OfficeInstaller"
 # ----------------------------------
 
